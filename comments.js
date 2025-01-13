058245b36433f392bc0987d3ac62d092f41fa794
Taylor Spengen <spengent@mcmaster.ca>
2025-01-13 05:20:45 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,12 @@
+// Create web server
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+    // Open a file on the server and return its content
+    fs.readFile('comments.html', function(err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    });
+}).listen(8080);
